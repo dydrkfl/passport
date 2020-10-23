1f87d20fb1dc75d4f56a2ad60b48b04cc9c019ba
= <qqfelix587@gmail.com>
2020-10-23 14:19:12 +0900

    lec12 Writing

--- routes/topic.js
+++ routes/topic.js
@@ -5,18 +5,18 @@ var fs = require('fs');
 var sanitizeHtml = require('sanitize-html');
 var template = require('../lib/template');
 const auth = require("../lib/auth");
-
-
+var db = require('../lib/db');
+var shortid = require('shortid')
 
 
 router.get('/create', function (request, response) {
-  if(!auth.isOwner(request,response)){
+  if (!auth.isOwner(request, response)) {
     response.redirect('/');
     return false;
   }
-    var title = 'WEB - create';
-    var list = template.list(request.list);
-    var html = template.HTML(title, list, `
+  var title = 'WEB - create';
+  var list = template.list(request.list);
+  var html = template.HTML(title, list, `
       <form action="/topic/create_process" method="post">
         <p><input type="text" name="title" placeholder="title"></p>
         <p>
@@ -27,51 +27,45 @@ router.get('/create', function (request, response) {
         </p>
       </form>
     `, '', auth.statusUI(request, response));
-    response.send(html);
-  
-  });
-  
-  
-  router.post('/create_process', function (request, response) {
-    // var body = '';
-    // request.on('data', function (data) {
-    //   body = body + data;
-    // });
-    // request.on('end', function () {
-    //   var post = qs.parse(body);
-    //   var title = post.title;
-    //   var description = post.description;
-    //   fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-    //     response.redirect(`/page/${title}`);
-    //   })
-    // });
-    if(!auth.isOwner(request,response)){
-      response.redirect('/');
-      return false;
-    }
-    var post = request.body;
-    var title = post.title;
-    var description = post.description;
-    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-      response.redirect(`/topic/${title}`);
-    })
-  })
-  
-  router.get('/update/:pageId', function (request, response, next) {
-    if(!auth.isOwner(request,response)){
-      response.redirect('/');
-      return false;
-    }
-      var filteredId = path.parse(request.params.pageId).base;
-      fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-        if(err){
-          next(err);
-        }
-        else{
-          var title = request.params.pageId;
-          var list = template.list(request.list);
-          var html = template.HTML(title, list,
-            `
+  response.send(html);
+
+});
+
+
+router.post('/create_process', function (request, response) {
+
+  if (!auth.isOwner(request, response)) {
+    response.redirect('/');
+    return false;
+  }
+  var post = request.body;
+  var title = post.title;
+  var description = post.description;
+  var id = shortid.generate();
+  db.get('topics').push({
+    id: id,
+    title: title,
+    description: description,
+    user_id: request.user.id
+  }).write();
+  response.redirect(`/topic/${id}`);
+
+})
+
+router.get('/update/:pageId', function (request, response, next) {
+  if (!auth.isOwner(request, response)) {
+    response.redirect('/');
+    return false;
+  }
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
+    if (err) {
+      next(err);
+    } else {
+      var title = request.params.pageId;
+      var list = template.list(request.list);
+      var html = template.HTML(title, list,
+        `
           <form action="/topic/update_process" method="post">
             <input type="hidden" name="id" value="${title}">
             <p><input type="text" name="title" placeholder="title" value="${title}"></p>
@@ -83,78 +77,77 @@ router.get('/create', function (request, response) {
             </p>
           </form>
           `,
-            `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`,
-            auth.statusUI(request, response)
-          );
-          response.send(html)
-       
-        }
-      });
-        
-    
-  });
-  
-  router.post('/update_process', function (request, response) {
-    if(!auth.isOwner(request,response)){
-      response.redirect('/');
-      return false;
+        `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`,
+        auth.statusUI(request, response)
+      );
+      response.send(html)
+
     }
-    var post = request.body;
-    var id = post.id;
-    var title = post.title;
-    var description = post.description;
-    
-    fs.rename(`data/${id}`, `data/${title}`, function (error) {
-      fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-        response.redirect(`/topic/${title}`);
-      })
-    });
-  
   });
-  
-  router.post('/delete_process', function (request, response) {
-    if(!auth.isOwner(request,response)){
-      response.redirect('/');
-      return false;
-    }
-    var post = request.body;
-    var id = post.id;
-    var filteredId = path.parse(id).base;
-  
-    fs.unlink(`data/${filteredId}`, function (error) {
-      response.redirect('/');
+
+
+});
+
+router.post('/update_process', function (request, response) {
+  if (!auth.isOwner(request, response)) {
+    response.redirect('/');
+    return false;
+  }
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+
+  fs.rename(`data/${id}`, `data/${title}`, function (error) {
+    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+      response.redirect(`/topic/${title}`);
     })
+  });
+
+});
+
+router.post('/delete_process', function (request, response) {
+  if (!auth.isOwner(request, response)) {
+    response.redirect('/');
+    return false;
+  }
+  var post = request.body;
+  var id = post.id;
+  var filteredId = path.parse(id).base;
+
+  fs.unlink(`data/${filteredId}`, function (error) {
+    response.redirect('/');
   })
-  router.get('/:pageId', function (request, response,next) {
-      
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-      if(err){
-        next(err);
-        // next('route') 사용시 아래 error catch code 동작
-      }
-      else{
-        
-      var title = request.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ['h1']
-      });
-      var list = template.list(request.list);
-      var html = template.HTML(sanitizedTitle, list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/topic/create">create</a>
+})
+
+router.get('/:pageId', function (request, response, next) {
+  var topic = db.get('topics').find({
+    id: request.params.pageId
+  }).value();
+  var user = db.get('users').find({
+    id: topic.user_id
+  }).value();
+  var sanitizedTitle = sanitizeHtml(topic.title);
+  var sanitizedDescription = sanitizeHtml(topic.description, {
+    allowedTags: ['h1']
+  });
+  var list = template.list(request.list);
+  var html = template.HTML(sanitizedTitle, list,
+    `<h2>${sanitizedTitle}</h2>${sanitizedDescription}
+    <p> by ${user.displayName}</p>`,
+    ` <a href="/topic/create">create</a>
           <a href="/topic/update/${sanitizedTitle}">update</a>
           <form action="/topic/delete_process" method="post">
             <input type="hidden" name="id" value="${sanitizedTitle}">
             <input type="submit" value="delete">
           </form>`,
-          auth.statusUI(request,response)
-      );
-      response.send(html)
-      }
-      
-    });
+    auth.statusUI(request, response)
+  );
+  response.send(html)
+
+
+
 });
 
-  module.exports = router;
+
+module.exports = router;
